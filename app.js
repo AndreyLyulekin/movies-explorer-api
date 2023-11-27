7563dff90dc4b7e6f0955064eef03e4003c371ce
[Andrey Lyulekin] <[Lyulekin@mail.ru]>
2023-11-26 22:25:55 -0300

    done with i deps

---
+++ app.js
@@ -0,0 +1,17 @@
+// IMPORT PACKAGES
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const { errors } = require('celebrate');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(DB_URL || 'mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
